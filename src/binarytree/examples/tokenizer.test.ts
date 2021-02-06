ce1d7b858effb53193648477cfe0260d001e7d9e
sy <sy.smythe@gmail.com>
2021-02-06 11:31:07 -0500

    Add Tokenizer and Peekable classes for updates to the simple exprression parser example code on BinaryTree. Removed unecessary boilerplate files

--- src/binarytree/examples/tokenizer.test.ts
+++ src/binarytree/examples/tokenizer.test.ts
@@ -1,7 +1,7 @@
-import { Peekable } from './tokenizer'
+import { isNumber } from './simpleparser';
+import { Peekable, Tokenizer } from './tokenizer'
 
-describe('tokenizer', () => {
-
+describe('Peekable', () => {
   /*
    * Peekable::next()
    */
@@ -54,7 +54,7 @@ describe('tokenizer', () => {
     expect(p.peek().value).toEqual('c')
     expect(p.next().value).toEqual('c')    
   }),
-  
+    
   test('Peekable.peek() should return an empty string and done == true when iterator is at last char', () => {
     const p = new Peekable('abcde')
 
@@ -70,6 +70,98 @@ describe('tokenizer', () => {
     const { done: pd, value: pv } = p.peek()
     expect(pv).toEqual('')
     expect(pd).toBe(true)
+  }),
+
+  test('Peekable.peek() should return a value and done == false when iterator is not at last char', () => {
+    const p = new Peekable('abcde')
+
+    expect(p.peek()).toMatchObject({ value: 'a', done: false })
+    
+    p.next()
+    expect(p.peek()).toMatchObject({ value: 'b', done: false })
+
+    p.next()
+    expect(p.peek()).toMatchObject({ value: 'c', done: false })
+
+    p.next()
+    expect(p.peek()).toMatchObject({ value: 'd', done: false })
   })
-  
+
+});
+
+
+describe('Tokenizer', () => {
+
+  test('.parseNumber() should handle integers of one digit', () => {
+    const p = new Peekable('')
+    const t = new Tokenizer('')
+
+    const result = t.parseNumber('1', p)
+    expect(result).toEqual( 1 )
+  }),
+
+  test('.parseNumber() should handle integers arbitrary digit length', () => {
+    const p = new Peekable('234567890')
+    const t = new Tokenizer('')
+
+    const result = t.parseNumber('1', p)
+    expect(result).toEqual( 1234567890 )
+  }),
+
+  test('.parseNumber() should handle floating-point numbers of arbitrary digit length', () => {
+    const t = new Tokenizer('')
+
+    const p1 = new Peekable('.1')
+    const r1 = t.parseNumber('3', p1)
+    expect(r1).toEqual(3.1)
+
+    const p2 = new Peekable('1.14')
+    const r2 = t.parseNumber('3', p2)
+    expect(r2).toEqual(31.14)    
+
+    const p3 = new Peekable('14.1416')
+    const r3 = t.parseNumber('3', p3)
+    expect(r3).toEqual(314.1416)    
+  }),
+
+  test('.parse() should handle a single numeric input', () => {
+    /* INTEGER */
+    const t1 = new Tokenizer('1')
+    const r1 = t1.parse()
+
+    expect(r1.length).toEqual(1)
+    expect(r1[0]).toEqual('1')
+
+    /* FLOAT */
+    const t2 = new Tokenizer('3.14')
+    const r2 = t2.parse()
+
+    expect(r2.length).toEqual(1)
+    expect(r2[0]).toEqual('3.14')
+  }),
+
+  test('.parse() should return an array of tokens from the input', () => {
+    const t1 = new Tokenizer('(1)')
+    const r1 = t1.parse()
+
+    expect(r1.length).toEqual(3)
+    expect(r1[0]).toEqual('(')
+    expect(r1[1]).toEqual('1')
+    expect(r1[2]).toEqual(')')
+
+    const t2 = new Tokenizer('(3 + (4 * 5))')
+    const r2 = t2.parse()
+
+    expect(r2.length).toEqual(9)
+    expect(r2[0]).toEqual('(')
+    expect(r2[1]).toEqual('3')
+    expect(r2[2]).toEqual('+')   
+    expect(r2[3]).toEqual('(')
+    expect(r2[4]).toEqual('4')
+    expect(r2[5]).toEqual('*')
+    expect(r2[6]).toEqual('5')
+    expect(r2[7]).toEqual(')')
+    expect(r2[8]).toEqual(')') 
+  })
+
 });
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export const add = (a: number, b: number) => a + b
-
-console.log('from /src/index.ts');
