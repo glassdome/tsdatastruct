fa8a2378d95a67e37c778f77faa53db3c8e31845
sy <sy.smythe@gmail.com>
2021-02-08 08:05:17 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,39 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  rootDir: '.',
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: [
     '/node_modules/'
   ],
-  verbose: true
+  verbose: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: [
+    "<rootDir>/node_modules"
+  ],
+  coverageReporters: [
+    "json",
+    "lcov",
+    "text"
+  ],
+  coverageThreshold: {
+    "global": {
+      "branches": 60,
+      "functions": 60,
+      "lines": 60,
+      "statements": 60
+    }
+  },
+  transform: {
+    ".ts": "ts-jest"
+  },
+  testRegex: [
+    "./test/.*\\.(test|spec)\\.ts$",
+    "./src/.*\\.(test|spec)\\.ts$"
+  ],
+  moduleFileExtensions: [
+    "ts",
+    "js",
+    "json"
+  ]
 }
